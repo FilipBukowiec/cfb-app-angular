555a5fced8083cd8751753a75b630afc360a0735
FilipBukowiec <102471937+FilipBukowiec@users.noreply.github.com>
2024-09-24 15:38:42 +0200

    add dynamic-marquee

--- src/app/components/news-ticker/news-ticker.component.ts
+++ src/app/components/news-ticker/news-ticker.component.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Marquee, loop, LoopReturn } from 'dynamic-marquee';
 
 @Component({
   selector: 'app-news-ticker',
-  standalone: true, 
+  standalone: true,
   templateUrl: './news-ticker.component.html',
   styleUrls: ['./news-ticker.component.scss'],
 })
-export class NewsTickerComponent {}
+export class NewsTickerComponent implements AfterViewInit {
+  @ViewChild('marquee') marqueeElement!: ElementRef<HTMLElement>;
+  marqueeInstance?: Marquee;
+  loopInstance?: LoopReturn;
+
+  ngAfterViewInit(): void {
+    const $marquee = this.marqueeElement.nativeElement;
+
+    // Inicjalizacja dynamic-marquee
+    this.marqueeInstance = new Marquee($marquee, {
+      rate: -80,
+    });
+
+    // Konfiguracja loopowania treści
+    this.loopInstance = loop(
+      this.marqueeInstance,
+      [
+        () => 'Contrary to popular belief, Lorem Ipsum is not simply random text',
+        () =>
+          'It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
+        () =>
+          'Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source',
+        () =>
+          'Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC',
+      ],
+      () => {
+        const $separator = document.createElement('div');
+        $separator.innerHTML = '&nbsp•&nbsp';
+        return $separator;
+      }
+    );
+  }
+}
+
+
