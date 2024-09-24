a1a322160c9342ec8b51500897766cb15696de7c
FilipBukowiec <102471937+FilipBukowiec@users.noreply.github.com>
2024-09-24 18:03:16 +0200

    repair ticker

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NewsTickerComponent } from '../news-ticker/news-ticker.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [NewsTickerComponent],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
