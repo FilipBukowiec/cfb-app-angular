4973c733ad2e5f60b7d0da734db68e521910899c
FilipBukowiec <102471937+FilipBukowiec@users.noreply.github.com>
2024-09-30 22:51:52 +0200

    start gym-schedule

--- src/app/components/gym-schedule/gym-schedule.component.ts
+++ src/app/components/gym-schedule/gym-schedule.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatOption, MatSelect } from '@angular/material/select';
 
 @Component({
   selector: 'app-gym-schedule',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatFormField, MatSelect, MatOption, MatLabel],
   templateUrl: './gym-schedule.component.html',
   styleUrl: './gym-schedule.component.scss'
 })
