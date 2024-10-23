630aaad65f95df7e4e8889fd7c6d7da228fd5e9f
FilipBukowiec <102471937+FilipBukowiec@users.noreply.github.com>
2024-10-23 18:34:04 +0200

    end side-bar

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -17,12 +17,12 @@ export class SideMenuComponent implements OnInit {
   isFullscreen: boolean = false;
   isLoggedIn: boolean = false;
   isOnStartPage: boolean = false;
-  shouldRefresh: boolean = false; // Flaga do kontrolowania odświeżania
+  shouldRefresh: boolean = false;
 
   constructor(
     private authService: AuthService,
     private router: Router,
-    private dataService: DataService
+    private dataService: DataService,
   ) {}
 
   ngOnInit(): void {
@@ -34,23 +34,12 @@ export class SideMenuComponent implements OnInit {
       .pipe(filter((event) => event instanceof NavigationEnd))
       .subscribe((event: NavigationEnd) => {
         this.isOnStartPage = event.url === '/start';
-
-        // Odświeżanie komponentów tylko jeśli flaga jest ustawiona
-        if (this.shouldRefresh) {
-          this.refreshComponents(['swiper', 'footer']);
-          this.shouldRefresh = false; // Resetujemy flagę
-        }
-
-        console.log('Nawigacja do:', event.url);
-        console.log('Czy na stronie startowej:', this.isOnStartPage);
       });
 
-    // Monitorowanie zmian pełnoekranowych
     document.addEventListener('fullscreenchange', () => {
       this.isFullscreen = this.checkFullscreen();
     });
 
-    // Monitorowanie zmiany rozmiaru okna
     window.addEventListener('resize', () => {
       this.isFullscreen = this.checkFullscreen();
     });
@@ -83,34 +72,39 @@ export class SideMenuComponent implements OnInit {
 
   refreshComponents(componentKeys: string[]): void {
     const newData: { [key: string]: boolean } = {};
-
-    // Ustaw na false, aby wymusić aktualizację
     componentKeys.forEach(key => {
-      newData[key] = false; // Ustawiamy na false, aby wymusić ukrycie komponentów
+      newData[key] = false;
     });
-
-    this.dataService.updateData(newData); // Aktualizacja danych w serwisie
-
-    // Ustaw na true po krótkim czasie, aby pokazać komponenty
+    this.dataService.updateData(newData); 
     setTimeout(() => {
       const updatedData: { [key: string]: boolean } = {};
       componentKeys.forEach(key => {
-        updatedData[key] = true; // Ustawiamy ponownie na true, aby komponenty były widoczne
+        updatedData[key] = true;
       });
-      this.dataService.updateData(updatedData); // Aktualizacja danych w serwisie
-    }, 100); // Opóźnienie 100 ms
+      this.dataService.updateData(updatedData); 
+    }, 100);
   }
 
   navigateToStart(): void {
-    // Jeśli na stronie innej niż '/start', przejdź do '/start' i odśwież komponenty,
-    // w przeciwnym razie tylko przejdź do '/start' bez odświeżania
     if (!this.isOnStartPage) {
-      console.log('Nawigacja do strony startowej');
-      this.shouldRefresh = true; // Ustawiamy flagę, aby odświeżyć komponenty
-      this.router.navigate(['/start']); // Przechodzimy na stronę startową
-    } else if (this.shouldRefresh) {
-      console.log('Odświeżanie komponentów na stronie startowej');
-      this.refreshComponents(['swiper', 'footer']); // Odświeżanie komponentów
+      this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+      this.router.onSameUrlNavigation = 'reload';
+      this.router.navigate(['/start']).then(() => {
+        this.shouldRefresh = true;
+      });
+    } else {
+      if (this.shouldRefresh) {
+        this.shouldRefresh = false;
+      }
+    }
+  }
+
+  onStartClick(event: MouseEvent): void {
+    if (this.isOnStartPage) {
+      this.refreshComponents(["swiper", 'footer']);
+      event.preventDefault();
+    } else {
+      this.navigateToStart();
     }
   }
 }
